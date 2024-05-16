f63101edee955f84d82302f710b1a15949215cf2
TheKingsident <kingsident@gmail.com>
2024-05-16 20:59:42 +0100

    Updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -25,11 +25,20 @@ class RedactingFormatter(logging.Formatter):
         """ format method
         """
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)            
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ filter_datum that returns the log message obfuscated"""
-    return re.sub(r'(?:^|{})({})(?:$|{})'.format(
-        separator, '|'.join(fields), separator), redaction, message)
+    lst = message.split(separator)
+
+    for f in fields:
+        for i in range(len(lst)):
+            if lst[i].startswith(f):
+                subst = f + '=' + redaction
+                lst[i] = re.sub(lst[i], '', lst[i])
+                lst[i] = subst
+    return separator.join(lst)
+
