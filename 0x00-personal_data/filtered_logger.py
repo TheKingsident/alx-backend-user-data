baa3189764e5db76d844f2316dca1d1bcb0bde3e
TheKingsident <kingsident@gmail.com>
2024-05-16 22:21:34 +0100

    Updated for task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """ 0. Regex-ing
 """
 
-import csv
-import re
 import logging
+import mysql.connector
+import os
+import re
 from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -58,3 +59,17 @@ def get_logger() -> logging.Logger:
     user_data_logger.addHandler(stream_handler)
 
     return user_data_logger
+
+
+def get_db():
+    """ returns a connector to the database
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    if not db_name:
+        raise ValueError("PERSONAL_DATA_DB_NAME environment variable is not set")
+
+    return mysql.connector.connect(username, password, host, db_name)
