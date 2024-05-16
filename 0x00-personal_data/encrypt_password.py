bafa6c2e7353bf77047a6d66106ef26d3dbde9b6
TheKingsident <kingsident@gmail.com>
2024-05-16 23:23:16 +0100

    Updated

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@
 import bcrypt
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password, which is a byte string
     """
     salt = bcrypt.gensalt()
