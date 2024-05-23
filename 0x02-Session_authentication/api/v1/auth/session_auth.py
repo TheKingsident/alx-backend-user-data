728552ce064911da35fec8ca1517fd6b2e9216cf
TheKingsident <kingsident@gmail.com>
2024-05-23 21:01:53 +0100

    Updated for task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -40,3 +41,16 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.__class__.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ current_user method
+        """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
