a108435d0ba06d1f1601cc3ec48931c056a34f50
TheKingsident <kingsident@gmail.com>
2024-05-23 14:32:23 +0100

    Updated for task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,11 +12,25 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ create session method
+        """Creates a session ID for a user_id
+
+        Args:
+            user_id (str): The user ID for which the session ID is created
+
+        Returns:
+            str: The created session ID, or None if user_id is invalid
         """
         if user_id is None or not isinstance(user_id, str):
             return None
+
         session_id = str(uuid4())
         self.__class__.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.__class__.user_id_by_session_id.get(session_id)
