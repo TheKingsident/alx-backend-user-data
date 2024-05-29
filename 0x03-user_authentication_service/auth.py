399008583ec5d1d5dfbd5167472a5252af1131e6
TheKingsident <kingsident@gmail.com>
2024-05-29 21:05:42 +0100

    Update for task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,7 +67,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-    
+
     def destroy_session(self, user_id: int):
         """Destroys a session with user_id
         """
