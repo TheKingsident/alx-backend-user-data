ba71e29b3b9f2c1de3e1629d06a99814fc8918d7
TheKingsident <kingsident@gmail.com>
2024-05-29 21:04:35 +0100

    Update for task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,6 +67,14 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+    
+    def destroy_session(self, user_id: int):
+        """Destroys a session with user_id
+        """
+        try:
+            self._db.update_user(user_id=user_id, session_id=None)
+        except NoResultFound:
+            return None
 
 
 def _hash_password(password: str) -> bytes:
