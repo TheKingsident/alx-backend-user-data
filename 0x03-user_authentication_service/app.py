7ebd9800d4f39f392e80be04fb54713523f08e47
TheKingsident <kingsident@gmail.com>
2024-05-29 21:21:54 +0100

    Update for task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,6 +3,7 @@
 """
 
 from flask import Flask, jsonify, request, abort, make_response
+from flask import redirect
 from auth import Auth
 
 
@@ -48,6 +49,19 @@ def login():
     response.set_cookie("session_id", session_id)
 
     return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """Logs the user out
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id=session_id)
+    if user is None:
+        return abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
