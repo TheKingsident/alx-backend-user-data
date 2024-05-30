8b4dd30ca3957d7d83de498fd6dd3373b72fd0c5
TheKingsident <kingsident@gmail.com>
2024-05-30 15:47:55 +0100

    Update for task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,6 +76,16 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user_id=user.id, reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password and returns the hashed password as bytes
