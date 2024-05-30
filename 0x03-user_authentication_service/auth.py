082dafc821e444440f4da582705d4eb425a8ce82
TheKingsident <kingsident@gmail.com>
2024-05-30 16:21:00 +0100

    Update for task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,15 +88,15 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Updates the user password
+        """Updates the user's password
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password)
+            hashed_password = self._hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
         except NoResultFound:
-            raise ValueError
+            raise ValueError("Invalid reset token")
 
 
 def _hash_password(password: str) -> bytes:
